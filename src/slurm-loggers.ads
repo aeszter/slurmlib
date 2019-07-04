f53dee9a4c3268e71e5e872da0e5f4d7bd479995
Ansgar Esztermann <aeszter@gwdg.de>
2019-07-04 10:45:59 +0200

    Add Logger type (Bug #3252)
    -use it for Node and Nodegroup;
    -use it to catch faulty Nodes when building a Nodegroup;
    -use it to catch Nodes with too many used cpus.

---
+++ src/slurm-loggers.ads
@@ -0,0 +1,17 @@
+with Slurm.Utils;
+package Slurm.Loggers is
+   type Logger is tagged private;
+
+   function Has_Errors (Object : Logger) return Boolean;
+   function Errors_Exist return Boolean;
+   procedure Record_Error (Object : in out Logger; Message : String);
+   procedure Record_Error (Message : String);
+   procedure Iterate_Errors (Object  : Logger;
+                             Process : not null access procedure (Message : String));
+   procedure Iterate_Errors (Process : not null access procedure (Message : String));
+
+private
+   type Logger is tagged record
+      Error_Log : Slurm.Utils.String_List;
+   end record;
+end Slurm.Loggers;
