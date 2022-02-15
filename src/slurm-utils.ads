72b0a085483b12bdc7d3e3128fc6189ae811f584
Ansgar Esztermann <aeszter@gwdg.de>
2022-02-15 11:00:13 +0100

    post-release version bump

--- src/slurm-utils.ads
+++ src/slurm-utils.ads
@@ -9,7 +9,7 @@ with Ada.Containers.Ordered_Sets;
 with Ada.Containers.Doubly_Linked_Lists;
 
 package Slurm.Utils is
-   Version : String := "0.12";
+   Version : String := "0.12.9";
    type Tri_State is (False, True, Undecided);
    type User_Name is new String (1 .. 8);
 
